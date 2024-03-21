802e0ae7007079dc1a3ebae611ecdfb10df57e70
yusef02 <youssef.bouziane2402@gmail.com>
2024-03-21 20:37:15 +0100

    aggiunte le varei route per le possibili pagine link della header nav

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
   return view('partials.home');
 })->name('home');
+Route::get('/about', function () {
+  return view('partials.about');
+})->name('about');
+Route::get('/comics', function () {
+  return view('partials.comics');
+})->name('comics');
+Route::get('/movies', function () {
+  return view('partials.movies');
+})->name('movies');
+Route::get('/tv', function () {
+  return view('partials.tv');
+})->name('tv');
+Route::get('/games', function () {
+  return view('partials.games');
+})->name('games');
+Route::get('/collectibles', function () {
+  return view('partials.collectibles');
+})->name('collectibles');
+Route::get('/videos', function () {
+  return view('partials.videos');
+})->name('videos');
+Route::get('/fans', function () {
+  return view('partials.fans');
+})->name('fans');
+Route::get('/news', function () {
+  return view('partials.news');
+})->name('news');
+Route::get('/shop', function () {
+  return view('partials.shop');
+})->name('shop');
+Route::get('/characters', function () {
+  return view('partials.characters');
+})->name('characters');
